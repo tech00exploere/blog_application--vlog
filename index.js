19be8e430669ce08f6fe03a731446d6d94f7389e
Priyanshu chandra <priyanshu2507.rjs@gmail.com>
2025-11-27 19:09:43 +0530

    Refactor MongoDB connection and server setup

--- index.js
+++ index.js
@@ -11,13 +11,11 @@ const userRoute = require("./router/user");
 const app = express();
 const port = 4578;
 
-//  MongoDB Connection
 mongoose
   .connect("mongodb://localhost:27017/blog-app")
   .then(() => console.log(" MongoDB connected"))
   .catch((err) => console.error("❌ DB connection error:", err));
 
-//  View Engine Setup (Use path.join for Windows-safe paths)
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -25,43 +23,34 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-//  Serve static files from /public (no need to include /public in EJS paths)
 app.use(express.static(path.join(__dirname, "public")));
 
-//  Authentication middleware
 app.use(checkAuth);
 
-//  Pass logged-in user to all templates
 app.use((req, res, next) => {
   res.locals.user = req.user || null;
   next();
 });
 
-//  Routes
 app.get("/", (req, res) => {
   res.render("home", { user: req.user });
 });
 
-// Redirects
 app.get("/signin", (req, res) => res.redirect("/user/signin"));
 app.get("/signup", (req, res) => res.redirect("/user/signup"));
 
-// Routers
 app.use("/user", userRoute);
 app.use("/blog", blogRoute);
 
-// Logout
 app.get("/logout", (req, res) => {
   res.clearCookie("token");
   res.redirect("/");
 });
 
-//  404 Handler (Optional but helpful)
 app.use((req, res) => {
   res.status(404).render("404");
 });
 
-//  Server Start
 app.listen(port, () => {
   console.log(`🚀 Server running at http://localhost:${port}`);
 });
